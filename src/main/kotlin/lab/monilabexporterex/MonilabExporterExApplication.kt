5084376953a0795309b901738bf8a2fd70c4ca08
khope <heemang.kim@hoenstai.tech>
2025-09-05 18:28:54 +0900

    monilab exporter ex;

---
+++ src/main/kotlin/lab/monilabexporterex/MonilabExporterExApplication.kt
@@ -0,0 +1,11 @@
+package lab.monilabexporterex
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MonilabExporterExApplication
+
+fun main(args: Array<String>) {
+    runApplication<MonilabExporterExApplication>(*args)
+}
---
+++ src/test/kotlin/lab/monilabexporterex/MonilabExporterExApplicationTests.kt
@@ -0,0 +1,13 @@
+package lab.monilabexporterex
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MonilabExporterExApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
