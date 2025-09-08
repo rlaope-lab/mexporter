98aac43a8384ceee23b9105cce08ea2b9dbe16c6
khope <heemang.kim@hoenstai.tech>
2025-09-08 16:49:46 +0900

    add jvm data metrics

---
+++ src/main/kotlin/lab/monilabexporterex/controller/HealthCheckController.kt
@@ -0,0 +1,12 @@
+package lab.monilabexporterex.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HealthCheckController {
+    @GetMapping("/health")
+    fun healthCheck(): String {
+        return "OK"
+    }
+}
