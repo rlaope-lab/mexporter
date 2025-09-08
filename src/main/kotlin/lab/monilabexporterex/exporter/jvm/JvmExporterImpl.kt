2a2d847381558386ec86666bab8a97fd33ec01e2
khope <heemang.kim@hoenstai.tech>
2025-09-08 17:21:30 +0900

    memory gc thread exporter default implementation

---
+++ src/main/kotlin/lab/monilabexporterex/exporter/jvm/JvmExporterImpl.kt
@@ -0,0 +1,24 @@
+package lab.monilabexporterex.exporter.jvm
+
+import lab.monilabexporterex.exporter.JvmExporter
+import java.net.InetAddress
+
+class JvmExporterImpl(
+    private val memoryExporter: MemoryExporter,
+    private val gcExporter: GcExporter,
+    private val threadExporter: ThreadExporter,
+    private val cpuExporter: CpuExporter,
+    private val classLoadingExporter: ClassLoadingExporter,
+    private val networkExporter: NetworkExporter,
+    private val applicationExporter: ApplicationExporter
+) : JvmExporter {
+    override fun getHostname(): String = InetAddress.getLocalHost().hostName
+
+    override fun getMemoryInfo() = memoryExporter.getMemoryInfo()
+    override fun getGcInfo() = gcExporter.getGcInfo()
+    override fun getThreadInfo() = threadExporter.getThreadInfo()
+    override fun getCpuInfo() = cpuExporter.getCpuInfo()
+    override fun getClassLoadingInfo() = classLoadingExporter.getClassLoadingInfo()
+    override fun getNetworkInfo() = networkExporter.getNetworkInfo()
+    override fun getApplicationInfo() = applicationExporter.getApplicationInfo()
+}
---
+++ src/main/kotlin/lab/monilabexporterex/exporter/jvm/memory/DefaultMemoryExporter.kt
@@ -0,0 +1,27 @@
+package lab.monilabexporterex.exporter.jvm.memory
+
+import lab.monilabexporterex.exporter.data.JvmMonitoringData
+import lab.monilabexporterex.exporter.jvm.MemoryExporter
+import java.lang.management.ManagementFactory
+
+class DefaultMemoryExporter : MemoryExporter {
+    override fun getMemoryInfo(): JvmMonitoringData.Memory {
+        val memoryBean = ManagementFactory.getMemoryMXBean()
+        val heap = memoryBean.heapMemoryUsage
+        val nonHeap = memoryBean.nonHeapMemoryUsage
+
+        val pools: Map<String, Long> = ManagementFactory.getMemoryPoolMXBeans()
+            .associate { it.name to it.usage.used }
+
+        return JvmMonitoringData.Memory(
+            used = heap.used + nonHeap.used,
+            max = (heap.max + nonHeap.max),
+            committed = heap.committed + nonHeap.committed,
+            eden = pools["PS Eden Space"] ?: 0,
+            survivor = pools["PS Survivor Space"] ?: 0,
+            old = pools["PS Old Gen"] ?: 0,
+            bufferPoolUsed = 0,
+            maxDirectMemorySize = Runtime.getRuntime().maxMemory()
+        )
+    }
+}
