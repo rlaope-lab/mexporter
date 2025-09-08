f3f1e328ff5a68acb5eddfa3bb99159e22c89045
khope <heemang.kim@hoenstai.tech>
2025-09-08 16:53:49 +0900

    strategy interfaces

--- src/main/kotlin/lab/monilabexporterex/api/HealthCheckController.kt
+++ src/main/kotlin/lab/monilabexporterex/api/HealthCheckController.kt
@@ -1,4 +1,4 @@
-package lab.monilabexporterex.controller
+package lab.monilabexporterex.api
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
