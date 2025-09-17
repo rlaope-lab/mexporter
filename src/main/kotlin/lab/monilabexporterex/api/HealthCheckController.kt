1834157481ac0dd1237ee75e59fcc3f95a519e11
신희성 <vvwv3092@gmail.com>
2025-09-17 15:25:05 +0900

    delete test

--- src/main/kotlin/lab/monilabexporterex/api/HealthCheckController.kt
+++ src/main/kotlin/lab/monilabexporterex/api/HealthCheckController.kt
@@ -1,11 +1,10 @@
 package lab.monilabexporterex.api
 
-import lab.monilabexporterex.exporter.jvm.application.DefaultApplicationExporter
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class HealthCheckController(private val defaultApplicationExporter: DefaultApplicationExporter) {
+class HealthCheckController {
     @GetMapping("/health")
     fun healthCheck(): String {
         return "OK"
