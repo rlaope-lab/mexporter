f4ade30b03872a9d2e7c1cb3f3197d2103c2eae8
신희성 <vvwv3092@gmail.com>
2025-09-18 19:33:32 +0900

    update: network, cpu test load logic

--- src/test/kotlin/lab/monilabexporterex/exporter/ExporterTest.kt
+++ src/test/kotlin/lab/monilabexporterex/exporter/ExporterTest.kt
@@ -10,6 +10,10 @@ import org.junit.jupiter.api.Test
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import java.io.OutputStream
+import java.net.HttpURLConnection
+import java.net.Socket
+import java.net.URL
 
 @SpringBootTest
 class ExporterTest {
@@ -36,14 +40,29 @@ class ExporterTest {
 
     @Test
     fun printJvmMetrics() {
-        val cpu = cpuExporter.getCpuInfo()
-        val application = applicationExporter.getApplicationInfo()
-        val network = networkExporter.getNetworkInfo()
-
         repeat(5) { i ->
             val memory = memoryExporter.getMemoryInfo()
             val gc = gcExporter.getGcInfo()
             val threads = threadExporter.getThreadInfo()
+            val cpu = cpuExporter.getCpuInfo()
+            val application = applicationExporter.getApplicationInfo()
+            val network = networkExporter.getNetworkInfo()
+
+            // cpu 부하
+            var sum = 0L
+            for (num in 1..1_000_000) {
+                sum += num
+            }
+
+            // network 부하
+            try {
+                Socket("localhost", 9999).use { socket ->
+                    val output: OutputStream = socket.getOutputStream()
+                    output.write("ping\n".toByteArray())
+                    output.flush()
+                }
+            } catch (_: Exception) {
+            }
 
             log.info("==== Sample #$i ====")
 
@@ -65,28 +84,26 @@ class ExporterTest {
                 threads.deadlockedCount, threads.cpuTime, threads.states
             )
 
+            log.info(
+                "[CPU] processUsage={} systemUsage={} uptime={} startTime={} uptime={} openFds={}",
+                cpu.processUsage, cpu.systemUsage, cpu.uptime,
+                cpu.startTime, cpu.uptime, cpu.openFds
+            )
+
+            log.info(
+                "[Application] httpRequestsCount={} httpLatency={} dbConnectionsActive={} dbConnectionsActive={} dbConnectionsMax={} queueTasksPending={} customMetrics={}",
+                application.httpRequestsCount, application.httpLatency, application.dbConnectionsActive,
+                application.dbConnectionsActive, application.dbConnectionsMax, application.queueTasksPending,
+                application.customMetrics
+            )
+
+            log.info(
+                "[Network] bytesSent={} bytesReceived={} preferIPv4={} openSockets={} tcpConnections={} tcpEstablished={}",
+                network.bytesSent, network.bytesReceived, network.preferIPv4,
+                network.openSockets, network.tcpConnections, network.tcpEstablished
+            )
+
             Thread.sleep(1000) // 1초 간격
         }
-
-        log.info("==== Single Sample ====")
-
-        log.info(
-            "[CPU] processUsage={} systemUsage={} uptime={} startTime={} uptime={} openFds={}",
-            cpu.processUsage, cpu.systemUsage, cpu.uptime,
-            cpu.startTime, cpu.uptime, cpu.openFds
-        )
-
-        log.info(
-            "[Application] httpRequestsCount={} httpLatency={} dbConnectionsActive={} dbConnectionsActive={} dbConnectionsMax={} queueTasksPending={} customMetrics={}",
-            application.httpRequestsCount, application.httpLatency, application.dbConnectionsActive,
-            application.dbConnectionsActive, application.dbConnectionsMax, application.queueTasksPending,
-            application.customMetrics
-        )
-
-        log.info(
-            "[Network] bytesSent={} bytesReceived={} preferIPv4={} openSockets={} tcpConnections={} tcpEstablished={}",
-            network.bytesSent, network.bytesReceived, network.preferIPv4,
-            network.openSockets, network.tcpConnections, network.tcpEstablished
-        )
     }
 }
